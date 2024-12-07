d6ebcc61a90c675ebc255b64dd492a263d16b0db
nibedanbhattacharjee97 <nibedanbhattacharjee97@gmail.com>
2024-12-07 21:23:37 +0530

    new

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from io import BytesIO
 @st.cache_data(hash_funcs={pd.DataFrame: lambda _: None})
 def load_data(file):
     df = pd.read_excel(file)
-    df.rename(columns={'bani_Manager_Column_Name': 'Manager Name', 'bani_SPOC_Column_Name': 'SPOC Name'}, inplace=True)
+    df.rename(columns={'Actual_Manager_Column_Name': 'Manager Name', 'Actual_SPOC_Column_Name': 'SPOC Name'}, inplace=True)
     return df
 
 # Function to create SQLite database table for appointments
@@ -76,9 +76,9 @@ def insert_booking(date, time_range, manager, spoc, booked_by):
 def update_another_database(file):
     df = pd.read_excel(file)
 
-    conn = sqlite3.connect('slide.db')
+    conn = sqlite3.connect('duplicate.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS bani
+    c.execute('''CREATE TABLE IF NOT EXISTS studentcap
                  (id INTEGER PRIMARY KEY AUTOINCREMENT,
                  cmis_id TEXT,
                  student_name TEXT,
@@ -86,28 +86,27 @@ def update_another_database(file):
                  center_name TEXT,
                  uploader_name TEXT,
                  verification_type TEXT,
-                 mode_of_verification TEXT,
-                 date_of_verification TEXT)''')
+                 mode_of_verification TEXT)''')
     conn.commit()
 
     for index, row in df.iterrows():
-        c.execute('''INSERT INTO bani (cmis_id, student_name, cmis_ph_no, center_name, uploader_name, verification_type, mode_of_verification, date_of_verification)
-                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', (row['CMIS ID'], row['Student Name'], row['CMIS PH No(10 Number)'],
-                                                row['Center Name'], row['Name Of Uploder'], row['Verification Type'], row['Mode Of Verification'], row['Date Of Verification']))
+        c.execute('''INSERT INTO studentcap (cmis_id, student_name, cmis_ph_no, center_name, uploader_name, verification_type, mode_of_verification)
+                     VALUES (?, ?, ?, ?, ?, ?, ?)''', (row['CMIS ID'], row['Student Name'], row['CMIS PH No(10 Number)'],
+                                                row['Center Name'], row['Name Of Uploder'], row['Verification Type'], row['Mode Of Verification']))
     conn.commit()
     conn.close()
 
     st.success('Data updated successfully!')
 
-# Function to download data from slide.db
+# Function to download data from duplicate.db
 def download_another_database_data():
-    conn = sqlite3.connect('slide.db')
-    df = pd.read_sql_query("SELECT * FROM bani", conn)
+    conn = sqlite3.connect('duplicate.db')
+    df = pd.read_sql_query("SELECT * FROM studentcap", conn)
     conn.close()
     
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="bani.csv">Download CSV</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="studentcap.csv">Download CSV</a>'
     st.markdown(href, unsafe_allow_html=True)
 
 # Function to generate HTML for calendar view with bookings highlighted
@@ -166,13 +165,13 @@ def generate_calendar(bookings):
 
     return calendar_html
 
-# Function to bulk delete data from bani table in slide.db by cmis_id
-def bulk_delete_bani(cmis_ids):
-    conn = sqlite3.connect('slide.db')
+# Function to bulk delete data from studentcap table in duplicate.db by cmis_id
+def bulk_delete_studentcap(cmis_ids):
+    conn = sqlite3.connect('duplicate.db')
     c = conn.cursor()
     
     for cmis_id in cmis_ids:
-        c.execute("DELETE FROM bani WHERE cmis_id = ?", (cmis_id,))
+        c.execute("DELETE FROM studentcap WHERE cmis_id = ?", (cmis_id,))
     
     conn.commit()
     conn.close()
@@ -189,8 +188,7 @@ def download_sample_excel():
         'Center Name': ['Center 1', 'Center 2', 'Center 3'],
         'Name Of Uploder': ['Uploader 1', 'Uploader 2', 'Uploader 3'],
         'Verification Type': ['Placement', 'Placement', 'Enrollment', ],
-        'Mode Of Verification': ['G-meet', 'Call', 'Call'],
-        'Date Of Verification': ['12-01-2024','12-01-2024','12-01-2024']
+        'Mode Of Verification': ['G-meet', 'Call', 'Call']
     }
 
     # Creating a DataFrame from the sample data
@@ -326,7 +324,7 @@ def main():
         cmis_ids_df = pd.read_csv(file)
         cmis_ids = cmis_ids_df['cmis_id'].tolist()
         if st.button('Delete Records'):
-            bulk_delete_bani(cmis_ids)
+            bulk_delete_studentcap(cmis_ids)
 
 # Run the app
 if __name__ == '__main__':
